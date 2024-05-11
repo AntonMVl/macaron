528dc64c8b8c698c69d4ba078084b3949a511f02
AMorohin <AMorohin@yandex.ru>
2024-05-11 10:00:10 +0300

    begin

--- app/components/Header/header.tsx
+++ app/components/Header/header.tsx
@@ -1,3 +1,32 @@
+import Link from 'next/link'
+
 export default function Header() {
-	return <h1 className='header'>header</h1>
+	return (
+		<header className='header'>
+			<ul className='header__list'>
+				<li className='header__item'>
+					<div className='header__link-container'>
+						<Link className='header__link' href='#'>
+							Гарантия свежести
+						</Link>
+						<Link className='header__link' href='#'>
+							Доставка и оплата
+						</Link>
+						<Link className='header__link' href='#'>
+							Оптовые поставки
+						</Link>
+						<Link className='header__link' href='#'>
+							Контакты
+						</Link>
+					</div>
+				</li>
+				<li className='header__item'>
+					<div className='header__city-container'>
+						<button className='header__location-button'></button>
+						<p className='header__city-info'>г. Сыктывкар</p>
+					</div>
+				</li>
+			</ul>
+		</header>
+	)
 }
